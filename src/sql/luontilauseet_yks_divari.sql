f817c8b241d3b8e8b29dd5280dceb41a26495a16
Jaakko Lintula <lintulajaakko@gmail.com>
2025-03-06 17:35:02 +0200

    luontilauseet

---
+++ src/sql/luontilauseet_yks_divari.sql
@@ -0,0 +1,33 @@
+CREATE TABLE DivariInfo (
+    nimi VARCHAR(150) NOT NULL,
+    osoite VARCHAR(150) NOT NULL,
+);
+
+CREATE TABLE TeosTyyppi (
+    id SERIAL PRIMARY KEY,
+    nimi VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE TeosLuokka (
+    id SERIAL PRIMARY KEY,
+    nimi VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE Teos (
+    id SERIAL PRIMARY KEY,
+    isbn VARCHAR(13) CHECK (char_length(isbn) IN (10, 13)),
+    nimi VARCHAR(150) NOT NULL,
+    tekija VARCHAR(150) NOT NULL,
+    hinta NUMERIC(10,2) NOT NULL,
+    paino INT NOT NULL,
+    tyyppiId INT REFERENCES TeosTyyppi(id) ON DELETE SET NULL,
+    luokkaId INT REFERENCES TeosLuokka(id) ON DELETE SET NULL
+);
+
+CREATE TABLE Nide (
+    id SERIAL PRIMARY KEY,
+    teosId NOT NULL REFERENCES Teos(id) ON DELETE CASCADE,
+    ostohinta NUMERIC(10,2),
+    myyntipvm DATE,
+    tila VARCHAR(7) NOT NULL CHECK (tila IN ('vapaa', 'varattu', 'myyty'))
+);
