954c701398c9df8c75ea65dfb8196e1ce64cb00f
Kian Moloney <104380978+k9du@users.noreply.github.com>
2025-03-11 13:01:20 +0200

    config backend (#2)
    Co-authored-by: Kian Moloney <qckimo@tie-tkannat.it.tuni.fi>

--- backend.js
+++ backend.js
@@ -1,23 +1,39 @@
 import express from 'express';
 import pg from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const { Pool } = pg;
 
 export const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
+    user: process.env.USER,
+    host: process.env.HOST,
+    database: process.env.DATABASE,
+    password: process.env.PASSWORD,
+    port: process.env.PORT
 });
 
 pool.on('connect', () => {
     console.log('Connected to postgresql database');
 });
 
+pool.on('error', (err) => {
+    console.error('Unexpected error on idle client', err);
+    process.exit(-1);
+});
 
 const backend = express();
+const port = process.env.SERVER_PORT || 3000; // 8069?
 
 backend.use(express.json());
 
 backend.get('/', (req, res) => {
     res.send('hello')
+});
+
+backend.listen(port, () => {
+    console.log(`Server running on port ${port}`);
 });
 
 export default backend;
