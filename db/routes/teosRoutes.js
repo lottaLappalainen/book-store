db588f40fa1489534fd2835df87fa189c23758db
Lotta Lappalainen <xglola@tie-tkannat.it.tuni.fi>
2025-04-20 18:53:48 +0300

    remove all unnecessary functions from book db

--- db/routes/teosRoutes.js
+++ db/routes/teosRoutes.js
@@ -6,19 +6,6 @@ export const setupTeosRoutes = (backend) => {
     /*                                    Teos                                    */
     /* -------------------------------------------------------------------------- */
 
-    /* ------------------------------- Create Teos ------------------------------ */
-    backend.post('/api/teos', async (req, res) => {
-        const { isbn, nimi, tekija, hinta, paino, tyyppiId, luokkaId } = req.body;
-
-        try {
-            const queryData = q.createTeos(isbn, nimi, tekija, hinta, paino, tyyppiId, luokkaId);
-            const result = await pool.query(queryData.text, queryData.values);
-            res.status(201).json(result.rows[0]);
-        } catch (error) {
-            res.status(400).json({ error: error.message });
-        }
-    });
-
     /* ------------------ Create Teos Keskusdivariin ja d1 ---------------------- */
     backend.post('/api/divari/teos', async (req, res) => {
         const { isbn, nimi, tekija, hinta, julkaisuvuosi, paino, tyyppiId, luokkaId, divariId } = req.body;
@@ -63,55 +50,6 @@ export const setupTeosRoutes = (backend) => {
     backend.get('/api/teos', async (req, res) => {
         try {
             const queryData = q.getAllTeoksetWithNideCount();
-            const result = await pool.query(queryData.text);
-            res.status(200).json(result.rows);
-        } catch (error) {
-            res.status(400).json({ error: error.message });
-        }
-    });
-
-    /* ------------------------------- Update Teos ------------------------------ */
-    backend.put('/api/teos/:id', async (req, res) => {
-        const { id } = req.params;
-        const { isbn, nimi, tekija, hinta, paino, tyyppiId, luokkaId } = req.body;
-
-        try {
-            const queryData = q.updateTeos(id, isbn, nimi, tekija, hinta, paino, tyyppiId, luokkaId);
-            const result = await pool.query(queryData.text, queryData.values);
-            res.status(200).json(result.rows[0]);
-        } catch (error) {
-            res.status(400).json({ error: error.message });
-        }
-    });
-
-    /* ------------------------------- Delete Teos ------------------------------ */
-    backend.delete('/api/teos/:id', async (req, res) => {
-        const { id } = req.params;
-
-        try {
-            const queryData = q.deleteTeos(id);
-            const result = await pool.query(queryData.text, queryData.values);
-            res.status(200).json(result.rows[0]);
-        } catch (error) {
-            res.status(400).json({ error: error.message });
-        }
-    });
-
-    /* --------------------------- Get Unique "Tyypit" -------------------------- */
-    backend.get('/api/tyypit', async (req, res) => {
-        try {
-            const queryData = q.getTyypit();
-            const result = await pool.query(queryData.text);
-            res.status(200).json(result.rows);
-        } catch (error) {
-            res.status(400).json({ error: error.message });
-        }
-    });
-
-    /* --------------------------- Get Unique "Luokat" -------------------------- */
-    backend.get('/api/luokat', async (req, res) => {
-        try {
-            const queryData = q.getLuokat();
             const result = await pool.query(queryData.text);
             res.status(200).json(result.rows);
         } catch (error) {
